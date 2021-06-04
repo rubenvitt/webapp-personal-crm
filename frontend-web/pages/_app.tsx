9838416ea8abd6dba995691e93bf2aaad03f06b8
Ruben Vitt <r.vitt@fme.de>
2021-06-04 23:17:47 +0200

    ✨(web-frontend): some implementations

--- frontend-web/pages/_app.tsx
+++ frontend-web/pages/_app.tsx
@@ -11,7 +11,6 @@ function MyApp({ Component, pageProps }) {
       <Layout {...pageProps}>
         <Component {...pageProps} />
       </Layout>
-      <Element />
       <ReactQueryDevtools panelProps={{ className: "z-50" }} />
     </QueryClientProvider>
   );
@@ -20,5 +19,13 @@ function MyApp({ Component, pageProps }) {
 export default MyApp;
 
 const Element = () => {
-  return <div className="bg-green-600" />;
+  return (
+    <>
+      <div className="bg-green-50 bg-green-100 bg-green-200 bg-green-300 bg-green-400 bg-green-500 bg-green-600 bg-green-700 bg-green-800 bg-green-900" />
+      <div className="bg-blue-50 bg-blue-100 bg-blue-200 bg-blue-300 bg-blue-400 bg-blue-500 bg-blue-600 bg-blue-700 bg-blue-800 bg-blue-900" />
+      <div className="bg-yellow-50 bg-yellow-100 bg-yellow-200 bg-yellow-300 bg-yellow-400 bg-yellow-500 bg-yellow-600 bg-yellow-700 bg-yellow-800 bg-yellow-900" />
+      <div className="bg-orange-50 bg-orange-100 bg-orange-200 bg-orange-300 bg-orange-400 bg-orange-500 bg-orange-600 bg-orange-700 bg-orange-800 bg-orange-900" />
+      <div className="bg-gray-50 bg-gray-100 bg-gray-200 bg-gray-300 bg-gray-400 bg-gray-500 bg-gray-600 bg-gray-700 bg-gray-800 bg-gray-900" />
+    </>
+  );
 };
