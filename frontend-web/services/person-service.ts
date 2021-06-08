ba8d74ea06bc01cc0d39d884b77ae01de7f7e60c
Ruben Vitt <r.vitt@fme.de>
2021-06-08 21:36:17 +0200

    ✨(web-frontend): some implementations

--- frontend-web/services/person-service.ts
+++ frontend-web/services/person-service.ts
@@ -2,13 +2,23 @@ import { Person, PersonDetails } from "../globals/interfaces";
 import axios from "../axios";
 
 export const findAllPersons: () => Promise<Person[]> = async () => {
-  return axios.get("/api/persons").then((value) => value.data);
+  return axios
+    .get("/api/persons")
+    .then((value) => value.data)
+    .catch(() => undefined);
 };
 
 export const findAllFavoritePersons: () => Promise<Person[]> = async () => {
-  return axios.get("/api/persons?filter=favorites").then((value) => value.data);
+  return axios
+    .get("/api/persons?filter=favorites")
+    .then((value) => value.data)
+    .catch(() => undefined);
 };
 
 export const findDetailsFor: (aPersonId: string) => Promise<PersonDetails> = (
   aPersonId
-) => axios.get("/api/persons/" + aPersonId).then((value) => value.data);
+) =>
+  axios
+    .get("/api/persons/" + aPersonId)
+    .then((value) => value.data)
+    .catch(() => undefined);
