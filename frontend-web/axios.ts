09bd4cd58cee861930d5b1198f646e2ee4add75f
Ruben Vitt <r.vitt@fme.de>
2021-06-08 21:13:46 +0200

    ✨(web-frontend): some implementations

--- frontend-web/axios.ts
+++ frontend-web/axios.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    process.env.API_HOST ||
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
-    "http://localhost:3000",
+    process.env.API_HOST || process.env.VERCEL_URL || "http://localhost:3000",
 });
 
 export default instance;
