b1a50faeafc8aac7a398bd2b657f27da95034517
Ruben Vitt <r.vitt@fme.de>
2021-06-28 13:16:14 +0200

    ✨(web-frontend): some implementations

--- frontend-web/axios.ts
+++ frontend-web/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/",
+  baseURL:
+    (process.env.NEXT_PUBLIC_API_URL ||
+      "http://localhost:" + (process.env.PORT || 3000)) + "/api",
 });
 
 export default instance;
--- frontend-web/services/person-service.ts
+++ frontend-web/services/person-service.ts
@@ -9,27 +9,27 @@ import axios from "../axios";
 
 export const findAllPersons: () => Promise<Person[]> = async () => {
   return axios
-    .get("/api/persons")
+    .get("/persons")
     .then((value) => value.data)
     .catch(() => undefined);
 };
 
 export const findAllFavoritePersons: () => Promise<Person[]> = async () => {
   return axios
-    .get("/api/persons?filter=favorites")
+    .get("/persons?filter=favorites")
     .then((value) => value.data)
     .catch(() => undefined);
 };
 
 export const findDetailsFor: (aPersonId: string) => Promise<PersonDetails> = (
   aPersonId
-) => axios.get("/api/persons/" + aPersonId).then((value) => value.data);
+) => axios.get("/persons/" + aPersonId).then((value) => value.data);
 
 export const createPerson: (aPerson: CreatePerson) => Promise<IdOnly> = async (
   aPerson
 ) => {
   return axios
-    .post<IdOnly>("/api/persons/create", aPerson)
+    .post<IdOnly>("/persons/create", aPerson)
     .then((value) => value.data)
     .catch(() => undefined);
 };
@@ -38,7 +38,7 @@ export const updatePerson: (aPerson: UpdatePerson) => Promise<IdOnly> = async (
   aPerson
 ) => {
   return axios
-    .put<IdOnly>("/api/persons/" + aPerson._id, aPerson)
+    .put<IdOnly>("/persons/" + aPerson._id, aPerson)
     .then((value) => value.data)
     .catch(() => undefined);
 };
