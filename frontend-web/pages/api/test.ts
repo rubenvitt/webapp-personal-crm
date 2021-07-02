c809494cdd40384c6b35b9803f7fa3c6ce68b835
Ruben Vitt <r.vitt@fme.de>
2021-07-02 19:11:33 +0200

    ✨(web-frontend): some implementations

--- frontend-web/pages/api/test.ts
+++ frontend-web/pages/api/test.ts
@@ -1,11 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-import { withApiAuthRequired } from "@auth0/nextjs-auth0";
-
-export default withApiAuthRequired(function (req, res) {
-  res.send(
+export default function (req, res) {
+  res.send(process.env.VERCEL_URL);
+  /*res.send(
     (process.env.NEXT_PUBLIC_VERCEL_URL &&
       "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) ||
       process.env.AUTH0_BASE_URL ||
       "http://localhost:" + (process.env.PORT ?? "3000")
-  );
-});
+  );*/
+}
