697eced696ab140d4afef525179b50c13cda8cf6
Ruben Vitt <r.vitt@fme.de>
2021-07-02 17:37:15 +0200

    ✨(web-frontend): some implementations

--- frontend-web/next.config.js
+++ frontend-web/next.config.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 module.exports = {
   images: {
     domains: ["images.unsplash.com", "randomuser.me"],
+  },
+  env: {
+    AUTH0_BASE_URL:
+      process.env.AUTH0_BASE_URL ??
+      process.env.VERCEL_URL ??
+      "http://localhost:" + (process.env.PORT ?? "3000"),
   },
 };
