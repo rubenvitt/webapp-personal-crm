254d013aa83c5a77baec75c354f4c4614f851dc1
Ruben Vitt <r.vitt@fme.de>
2021-07-02 18:02:12 +0200

    ✨(web-frontend): some implementations

---
+++ frontend-web/pages/api/test.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function (req, res) {
+  res.send(
+    process.env.NEXT_PUBLIC_VERCEL_URL ||
+      process.env.AUTH0_BASE_URL ||
+      "http://localhost:" + (process.env.PORT ?? "3000")
+  );
+}
