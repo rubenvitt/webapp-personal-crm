96fb39f41fc59ef76c581630096b768f222490bc
Ruben Vitt <r.vitt@fme.de>
2021-07-02 18:08:00 +0200

    ✨(web-frontend): some implementations

--- frontend-web/globals/auth0.ts
+++ frontend-web/globals/auth0.ts
@@ -2,7 +2,8 @@ import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export default initAuth0({
   baseURL:
+    (process.env.NEXT_PUBLIC_VERCEL_URL &&
+      "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) ||
     process.env.AUTH0_BASE_URL ||
-    process.env.NEXT_PUBLIC_VERCEL_URL ||
     "http://localhost:" + (process.env.PORT ?? "3000"),
 });
