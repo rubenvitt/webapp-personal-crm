961fd233d1ff9e05cd2b5446309b141990aaeda5
Ruben Vitt <r.vitt@fme.de>
2021-07-02 19:27:07 +0200

    ✨(web-frontend): some implementations

--- frontend-web/globals/auth0.ts
+++ frontend-web/globals/auth0.ts
@@ -1,9 +1,21 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
+export function getBaseUrl() {
+  switch (process.env.VERCEL_ENV) {
+    case "production":
+      return "https://crm.rubeen.dev";
+    case "preview":
+      return "https://dev.crm.rubeen.dev";
+    default:
+      return (
+        (process.env.NEXT_PUBLIC_VERCEL_URL &&
+          "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) ||
+        process.env.AUTH0_BASE_URL ||
+        "http://localhost:" + (process.env.PORT ?? "3000")
+      );
+  }
+}
+
 export default initAuth0({
-  baseURL:
-    (process.env.NEXT_PUBLIC_VERCEL_URL &&
-      "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) ||
-    process.env.AUTH0_BASE_URL ||
-    "http://localhost:" + (process.env.PORT ?? "3000"),
+  baseURL: getBaseUrl(),
 });
--- frontend-web/pages/api/test.ts
+++ frontend-web/pages/api/test.ts
@@ -1,9 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function (req, res) {
-  /*res.send(
-    (process.env.NEXT_PUBLIC_VERCEL_URL &&
-      "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) ||
-      process.env.AUTH0_BASE_URL ||
-      "http://localhost:" + (process.env.PORT ?? "3000")
-  );*/
+import { getBaseUrl } from "../../globals/auth0";
+
+export default function () {
+  return getBaseUrl();
 }
