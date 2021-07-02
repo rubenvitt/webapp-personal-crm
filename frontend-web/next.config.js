8474e5b588c7bf95884de83e8610443bec4b6f77
Ruben Vitt <r.vitt@fme.de>
2021-07-02 17:45:28 +0200

    ✨(web-frontend): some implementations

--- frontend-web/next.config.js
+++ frontend-web/next.config.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   env: {
     AUTH0_BASE_URL:
-      process.env.AUTH0_BASE_URL ??
-      process.env.VERCEL_URL ??
+      process.env.VERCEL_URL ||
+      process.env.AUTH0_BASE_URL ||
       "http://localhost:" + (process.env.PORT ?? "3000"),
   },
 };
