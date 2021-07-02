2c1962da248f555ac73dea8021e67bae3995b2cb
Ruben Vitt <r.vitt@fme.de>
2021-07-02 19:35:25 +0200

    ✨(web-frontend): some implementations

--- frontend-web/globals/auth0.ts
+++ frontend-web/globals/auth0.ts
@@ -1,11 +1,9 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
-export function getBaseUrl() {
+export const getBaseUrl = (): string => {
   switch (process.env.VERCEL_ENV) {
     case "production":
       return "https://crm.rubeen.dev";
-    case "preview":
-      return "https://dev.crm.rubeen.dev";
     default:
       return (
         (process.env.NEXT_PUBLIC_VERCEL_URL &&
@@ -14,7 +12,7 @@ export function getBaseUrl() {
         "http://localhost:" + (process.env.PORT ?? "3000")
       );
   }
-}
+};
 
 export default initAuth0({
   baseURL: getBaseUrl(),
--- frontend-web/pages/api/test.ts
+++
@@ -1,5 +0,0 @@
-import { getBaseUrl } from "../../globals/auth0";
-
-export default function () {
-  return getBaseUrl();
-}
