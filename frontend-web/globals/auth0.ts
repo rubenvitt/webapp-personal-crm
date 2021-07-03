d63596546dc5588cb0eec933105f63687108b871
Ruben Vitt <r.vitt@fme.de>
2021-07-03 18:40:29 +0200

    ✨(web-frontend): some implementations

--- frontend-web/globals/auth0.ts
+++ frontend-web/globals/auth0.ts
@@ -1,4 +1,5 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
+import { ManagementClient } from "auth0";
 
 export const getBaseUrl = (): string => {
   switch (process.env.VERCEL_ENV) {
@@ -16,4 +17,16 @@ export const getBaseUrl = (): string => {
 
 export default initAuth0({
   baseURL: getBaseUrl(),
+  authorizationParams: {
+    audience: "https://r-personal-crm.eu.auth0.com/api/v2/",
+    scope:
+      "openid profile read:current_user update:current_user_metadata read:users",
+  },
+});
+
+export const managementClient = new ManagementClient({
+  domain: process.env.API_AUTH0_CLIENT_DOMAIN,
+  clientId: process.env.API_AUTH0_CLIENT_ID,
+  clientSecret: process.env.API_AUTH0_CLIENT_SECRET,
+  scope: "read:users update:users",
 });
---
+++ frontend-web/pages/api/user/index.ts
@@ -0,0 +1,29 @@
+import nextConnect from "next-connect";
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
+import { managementClient } from "../../../globals/auth0";
+
+const handler = nextConnect();
+
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
+  const { user: sessionUser } = getSession(req, res);
+  await managementClient.updateUser(
+    {
+      id: sessionUser.sub,
+    },
+    {
+      user_metadata: {
+        online_test: "success",
+      },
+      email_verified: false,
+    }
+  );
+
+  const user = await managementClient.getUser({
+    id: sessionUser.sub,
+  });
+
+  res.send(user);
+});
+
+export default withApiAuthRequired(handler);
