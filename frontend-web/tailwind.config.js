1504c458ff9139012c3db2287a018c5b5f3ae1c0
Ruben Vitt <r.vitt@fme.de>
2021-07-05 16:59:16 +0200

    ✨(web-frontend): some implementations

--- frontend-web/tailwind.config.js
+++ frontend-web/tailwind.config.js
@@ -4,31 +4,21 @@ const colors = require("tailwindcss/colors");
 // eslint-disable-next-line no-undef
 module.exports = {
   purge: {
-    safelist: [
-      ...["text", "bg"]
-        .map((a) => {
-          return ["red", "orange", "green", "gray", "yellow", "primary"].map(
-            (b) => {
-              return [50, 100, 200, 300, 400, 500, 600, 700, 800, 900].map(
-                (c) => {
-                  return a + "-" + b + "-" + c;
-                }
-              );
-            }
-          );
-        })
-        .flat()
-        .flat(),
-    ],
+    safelist: {
+      deep: [
+        /red/,
+        /orange/,
+        /green/,
+        /gray/,
+        /yellow/,
+        /primary/,
+        /(from|via|to|border|bg|text)-(.*)-(\\d0{1,2})/,
+      ],
+    },
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    options: {
-      safelist: {
-        deep: [/(from|via|to|border|bg|text)-(.*)-(\\d{1}0{1,2})/],
-      },
-    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
