3188d29b2b0139838de1e936c276b9845e5291ae
Ruben Vitt <r.vitt@fme.de>
2021-08-01 20:40:23 +0200

    ✨(general): some improvements

--- frontend-web/pages/api/persons/[id]/index.ts
+++ frontend-web/pages/api/persons/[id]/index.ts
@@ -26,18 +26,16 @@ handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
   Logger.log("I am here");
   res.status(200).send({});
 
-  if (false) {
-    Logger.log("update user:", req.body);
+  Logger.log("update user:", req.body);
 
-    const result = await apiUpdatePerson(req.body);
-    Logger.log("returning:", result);
-    if (result.n === result.ok) {
-      await res.status(200).send(null);
-    } else {
-      await res.status(400).json({
-        status: "Person with id " + req.body._id + " not updated",
-      });
-    }
+  const result = await apiUpdatePerson(req.body);
+  Logger.log("returning:", result);
+  if (result.n === result.ok) {
+    await res.status(200).send(null);
+  } else {
+    await res.status(400).json({
+      status: "Person with id " + req.body._id + " not updated",
+    });
   }
 });
 
