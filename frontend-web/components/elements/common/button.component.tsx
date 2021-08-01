66a0a048b06103e62f9746229d991937a24fcd2a
Ruben Vitt <r.vitt@fme.de>
2021-08-01 22:14:48 +0200

    🐛(edit): fix default button for contact-edit

--- frontend-web/components/elements/common/button.component.tsx
+++ frontend-web/components/elements/common/button.component.tsx
@@ -26,6 +26,7 @@ export function Button({
   isLoading,
   customColor,
   isDisabled,
+  type = "button",
   ...rest
 }: Props): JSX.Element {
   const [_isLoading, setLoading] = useState(false);
@@ -63,6 +64,7 @@ export function Button({
         className,
         "inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 "
       )}
+      type={type}
       {...rest}
     >
       {children}
